a1b9ddd5f5ab114018d954cee531af114aa91969
Alex <zuedev@gmail.com>
2024-06-18 16:22:32 +0100

    remove guestbook stuff

--- cloudflare/_worker.js
+++ cloudflare/_worker.js
@@ -8,23 +8,6 @@ export default {
           return Respond("Hello, world!");
         case "/_worker/goodbye":
           return Respond("Goodbye, world!");
-        case "/_worker/guestbook/get":
-          const getGuestbook = await environment.KV.get("guestbook");
-          return Respond(getGuestbook);
-        case "/_worker/guestbook/add":
-          const { name, message } = await request.json();
-
-          let guestbook = await environment.KV.get("guestbook");
-
-          if (guestbook) {
-            guestbook.push({ name, message });
-          } else {
-            guestbook = [{ name, message }];
-          }
-
-          await environment.KV.put("guestbook", guestbook);
-
-          return Respond(guestbook);
         default:
           return Respond("Not found", { status: 404 });
       }
