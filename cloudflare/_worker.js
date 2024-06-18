902d34d4e76cb9904a425a80a0703c3125232784
Alex <zuedev@gmail.com>
2024-06-18 01:36:42 +0100

    change api to _workerHandler

--- cloudflare/_worker.js
+++ cloudflare/_worker.js
@@ -2,17 +2,18 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname.startsWith("/api/")) return apiHandler(url.pathname);
+    if (url.pathname.startsWith("/_worker/"))
+      return _workerHandler(url.pathname);
 
     return env.ASSETS.fetch(request);
   },
 };
 
-function apiHandler(pathname) {
+function _workerHandler(pathname) {
   switch (pathname) {
-    case "/api/hello":
+    case "/_worker/hello":
       return Respond("Hello, world!");
-    case "/api/goodbye":
+    case "/_worker/goodbye":
       return Respond("Goodbye, world!");
     default:
       return Respond("Not found", { status: 404 });
