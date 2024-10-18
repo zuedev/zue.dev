6803ac33f9e27e963f6728382587c31c2e629968
zuedev <zuedev@gmail.com>
2024-10-18 19:41:16 +0100

    add what am I up to?

--- src/app/page.js
+++ src/app/page.js
@@ -9,9 +9,9 @@ export default () => {
             I'm <pan>zuedev</pan>.
           </h1>
         </section>
-        <section className="border-l-4 border-black dark:border-white">
+        <section className="border-4 border-black dark:border-white">
           <h2 className="text-6xl font-bold bg-black text-white dark:bg-white dark:text-black p-2">
-            Who am I?
+            Who am I? ✨
           </h2>
 
           <div className="space-y-4 p-4 text-justify">
@@ -80,6 +80,91 @@ export default () => {
                 music and programming something, probably working on my latest
                 game or open-source project.
               </p>
+            </div>
+          </div>
+        </section>
+        <section className="bg-gradient-to-br from-yellow-400 to-pink-400 text-black dark:from-blue-700 dark:to-cyan-400 dark:text-white p-4 space-y-4">
+          <h2 className="text-6xl font-bold">What am I up to? 💼</h2>
+
+          <div className="text-justify space-y-4">
+            <p>
+              I'm always working on something... There's not enough time in the
+              day to do everything I want to do! Here's a few things I'm
+              currently working on:
+            </p>
+
+            <div className="grid grid-cols-1 xl:grid-cols-3 gap-4">
+              {[
+                {
+                  title: "For Money 💵",
+                  description: (
+                    <>
+                      Money makes the world go round, unfortunately... That's
+                      why I founded and run both{" "}
+                      <a
+                        className="underline decoration-wavy text-orange-500 dark:text-yellow-300"
+                        href="https://termina.one"
+                        target="_blank"
+                      >
+                        Termina One
+                      </a>{" "}
+                      and{" "}
+                      <a
+                        className="underline decoration-wavy text-green-700 dark:text-green-400"
+                        href="https://area96.digital"
+                        target="_blank"
+                      >
+                        Area96 Digital
+                      </a>{" "}
+                      to help build capital for my other projects.
+                    </>
+                  ),
+                },
+                {
+                  title: "For Fun 🎉",
+                  description: (
+                    <>
+                      What don't I do? I have a backlog of games to play, music
+                      to make, and projects to work on. Right now, I'm trying to
+                      get back into making mods for{" "}
+                      <a
+                        className="underline decoration-wavy text-pink-500 dark:text-red-500"
+                        href="https://minecraft.net"
+                        target="_blank"
+                      >
+                        Minecraft
+                      </a>
+                      .
+                    </>
+                  ),
+                },
+                {
+                  title: "For Charity ❤️",
+                  description: (
+                    <>
+                      I'm currently working on directing a non-profit project
+                      called{" "}
+                      <a
+                        className="underline decoration-wavy text-red-500 dark:text-pink-400"
+                        href="https://unnamed.group"
+                        target="_blank"
+                      >
+                        Unnamed Group
+                      </a>{" "}
+                      to help support people online who are looking for a safe
+                      space to play games and socialise.
+                    </>
+                  ),
+                },
+              ].map((item, index) => (
+                <div
+                  key={index}
+                  className="bg-white text-black dark:bg-black dark:text-white p-4 space-y-2"
+                >
+                  <h3 className="text-4xl font-bold">{item.title}</h3>
+                  <p>{item.description}</p>
+                </div>
+              ))}
             </div>
           </div>
         </section>
