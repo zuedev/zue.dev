ee8d3549cb9dccc8b7fe839635c0ec3fa30c9109
Alex Pooley <zuedev@gmail.com>
2025-03-19 05:45:48 +0000

    feat: enhance middleware by adding pretty JSON responses and request ID

--- source/index.js
+++ source/index.js
@@ -1,12 +1,20 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { prettyJSON } from "hono/pretty-json";
+import { requestId } from "hono/request-id";
 
 import service_status from "./library/service_status";
 
 const app = new Hono();
 
 // enable cors for all routes
-app.use("*", cors());
+app.use(cors());
+
+// pretty print json responses
+app.use(prettyJSON());
+
+// add a request id to all requests
+app.use(requestId());
 
 // return a simple hello world message
 app.get("/", (context) => {
