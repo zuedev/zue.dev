dfea00b8e120378463f12c96a49dabf7b04aaebb
Alex Pooley (@zuedev) <zuedev@gmail.com>
2025-07-18 03:51:45 +0100

    metadata + tab animation

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,27 @@
 import "./globals.css";
 
+import Script from "next/script";
+
+export const metadata = {
+  title: "zuedev's space",
+  description: "Hello, World! I'm zuedev.",
+  icons: {
+    icon: "https://about.zue.dev/avatar.png",
+  },
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export default ({ children }) => {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Script src="/tabAnimation.js" />
     </html>
   );
 };
