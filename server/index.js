1279a995a1498222d3ea0d66c050a2f22d7fc8f9
Alex Pooley (@zuedev) <zuedev@gmail.com>
2025-09-04 21:53:26 +0100

    refactor API response handling to include default case for not found

--- server/index.js
+++ server/index.js
@@ -3,9 +3,21 @@ export default {
     const url = new URL(request.url);
 
     if (url.pathname.startsWith("/api")) {
-      return new Response(JSON.stringify({ message: "Hello, World! :3" }), {
-        headers: { "Content-Type": "application/json" },
-      });
+      const apiPath = url.pathname.split("/").filter(Boolean);
+
+      if (!apiPath[1]) apiPath[1] = "index";
+
+      switch (apiPath[1]) {
+        case "index":
+          return new Response(JSON.stringify({ message: "Hello, World! :3" }), {
+            headers: { "Content-Type": "application/json" },
+          });
+        default:
+          return new Response(JSON.stringify({ message: "Not Found" }), {
+            status: 404,
+            headers: { "Content-Type": "application/json" },
+          });
+      }
     }
 
     return env.ASSETS.fetch(request);
